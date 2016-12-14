1d6b20bfd0c86e387378e36ecd173a34e087aa09
Ubuntu <ubuntu@ip-172-31-27-158.eu-west-1.compute.internal>
2016-12-14 14:04:36 +0000

    Updated shell script

--- elkconfig.sh
+++ elkconfig.sh
@@ -10,6 +10,7 @@ sudo apt-get install -y elasticsearch
 
 sudo service elasticsearch restart
 
+sudo update-rc.d elasticsearch defaults 95 10
 ##Test to see it works##
 #curl localhost:9200
 
@@ -30,10 +31,15 @@ sudo update-rc.d logstash defaults 97 8
 ##Redirect System logs to Logstash##
 
 sudo cp ~/ELK-stack/10-syslog.conf /etc/logstash/conf.d
-#sudo service logstash restart
+sudo service logstash restart
 
 ##Install kibana##
 
 sudo wget https://download.elastic.co/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz
 sudo tar zxvf kibana-4.1.1-linux-x64.tar.gz
 
+sudo mkdir -p kibana-4.1.1-linux-x64/* /opt/kibana
+cd /etc/init.d && sudo wget https://raw.githubusercontent.com/akabdog/scripts/master/kibana4_init -O kibana4
+sudo chmod +x /etc/init.d/kibana4
+sudo update-rc.d kibana4 defaults 96 9
+sudo service kibana4 start
